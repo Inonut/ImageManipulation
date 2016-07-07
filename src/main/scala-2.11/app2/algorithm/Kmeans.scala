e7caf201632dc49aa440b520d26e7a468438deee
Dragos Raducanu <dragos.raducanu@kepler-rominfo.com>
2016-07-07 17:27:00 +0300

    algotitm

---
+++ src/main/scala-2.11/app2/algorithm/Kmeans.scala
@@ -0,0 +1,8 @@
+package app2.algorithm
+
+/**
+  * Created by Dragos on 7/7/2016.
+  */
+class Kmeans extends Means{
+
+}
---
+++ src/main/scala-2.11/app2/algorithm/Means.scala
@@ -0,0 +1,8 @@
+package app2.algorithm
+
+/**
+  * Created by Dragos on 7/7/2016.
+  */
+trait Means {
+
+}
