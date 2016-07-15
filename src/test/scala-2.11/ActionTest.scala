44229738510fc38aeebd79fb6c2bdabefc982846
Dragos Raducanu <dragos.raducanu@kepler-rominfo.com>
2016-07-15 17:01:56 +0300

    nu merge...

--- src/test/scala-2.11/ActionTest.scala
+++ src/test/scala-2.11/ActionTest.scala
@@ -3,28 +3,35 @@ import app2.action.ActionControl
 import scala.concurrent._
 import scala.concurrent.duration._
 
+import scala.util.Try
+import scala.concurrent.Future
+
+import ExecutionContext.Implicits.global
+
 /**
   * Created by Dragos on 7/15/2016.
   */
 object ActionTest {
 
+  def launch(f: () => Unit, timeout: Int): Future[Try[Unit]] = {
+
+    val aref = new java.util.concurrent.atomic.AtomicReference[Thread]()
+    val cdl = new java.util.concurrent.CountDownLatch(1)
+
+    Future {Thread.sleep(timeout); cdl.await(); aref.get().interrupt()}   // 1
+    Future {aref.set(Thread.currentThread); cdl.countDown(); Try(f())}  // 2
+  }
+
+
   def main(args: Array[String]) {
 
-    val action = ActionControl[Any, Any] {
+    launch(() => {
       for(i <- 1 until 1000) {
         Thread.sleep(10)
         println(i)
       }
-    }
-
-    action.execute()
-
-    Thread.sleep(1000)
-
-    try {
-      action.distroyEvent.fire()
-    } catch {
-      case e: Throwable => println(e)
+    }, 1000) recover {
+      case e => println(e)
     }
 
     Thread.sleep(100000)
